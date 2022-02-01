6836ed356cc23cba5c3ebf06d6533e3beedccc58
ThinEi <4111438+thineikhaing@users.noreply.github.com>
2022-02-02 01:05:02 +0800

    Create 9_guthrieIndex.java

---
+++ 9_guthrieIndex.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q9GuthrieIndex {
+
+    public static void main(String[] args) {
+        System.out.println(guthrieIndex(7));
+    }
+
+    static int guthrieIndex(int n) {
+      if (n == 1){
+        return 0;
+      }
+      
+      List<Integer> list = new ArrayList<>();
+      while(n!=0 && n!=1){
+        if(n%2 == 0){
+          n = n / 2;
+        }else{
+          n = 3*n +1;
+        }
+        
+        list.add(n);
+      }
+      
+      if(list.get(list.size() - 1) == 0){
+        return 0;
+      }else{
+        return list.size();
+      }
+        
+    }
+}
